f87e02171dfb781c5c4f79049c98e7b501c10ea0
Aaryush1 <aaryush@outlook.com>
2022-12-07 11:43:42 -0600

    Redeployment

--- components/ResponsiveAppBar.tsx
+++ components/ResponsiveAppBar.tsx
@@ -112,6 +112,7 @@ function ResponsiveAppBar() {
                         }}
                     >
                         <Image src={carLogo} alt="logo" />
+
                     </Typography>
                     <Box sx={{
                         flexGrow: 1, display: { xs: 'none', md: 'flex' }, alignItems: 'center',
